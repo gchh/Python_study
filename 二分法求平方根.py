e10ecdda65ab18150e454a3f46f3310e90e7092b
gchh <skring.2008@163.com>
2017-03-25 17:07:09 +0800

    17-3-25-17h

--- 二分法求平方根.py
+++ 二分法求平方根.py
@@ -1,12 +1,28 @@
 x=float(input())
-low=0
-high=x
-guess=(low+high)/2
-
-while abs(guess**2-x)>1e-5:
-    if guess**2<x:
-        low=guess
-    else:
-        high=guess
+y=abs(x)
+if y>=1:
+    low=0
+    high=y
     guess=(low+high)/2
-print('The root of %f is: %f'%(x,guess))
+
+    while abs(guess**2-y)>1e-5:
+        if guess**2<y:
+            low=guess
+        else:
+            high=guess
+        guess=(low+high)/2
+else:
+    low=y
+    high=1
+    guess=(low+high)/2
+
+    while abs(guess**2-y)>1e-5:
+        if guess**2>y:
+            high=guess
+        else:
+            low=guess
+        guess=(low+high)/2
+if x>=0:
+    print('The root of %f is: ±%f'%(x,guess))
+else:
+    print('The root of %f is: ±%fi'%(x,guess))
