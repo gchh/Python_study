61a2ceb263c014ad85ff912b5686c41f014152ed
gchh <skring.2008@163.com>
2017-03-27 20:01:33 +0800

    17-3-27-20h

--- 汉诺塔游戏.py
+++ 汉诺塔游戏.py
@@ -29,3 +29,16 @@ def move(n,a,b,c):
 if __name__=='__main__':
     n = int(input())
     move(n,'A','B','C')
+'''
+#Python2.7 print和3.x不同
+def hanoi(n, A, B, C):
+    if n == 1:
+        print 'Move', n, 'from', A, 'to', C
+    else:
+        hanoi(n - 1, A, C, B)
+        print 'Move', n, 'from', A, 'to', C
+        hanoi(n - 1, B, A, C)
+ 
+num = int(raw_input())
+hanoi(num, 'A', 'B', 'C')
+'''
